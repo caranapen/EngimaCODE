0eefb20aea38e5ae4a0cb3ae001759fabfd16789
Daniel Rodca <dani.kn17.rjch@gmail.com>
2014-11-26 09:24:22 -0400

    Puesto un boton de login y otro de registro. Pero no van los eventos de clickearlos, sabre porque

---
+++ EnigmaCode/EnigmaCode.js
@@ -0,0 +1,38 @@
+if (Meteor.isClient) {
+
+    Session.setDefault("counter1", 0);
+    Session.setDefault("counter2", 0);
+    
+    Template.login.helpers({
+        counter: function () {
+            return Session.get("counter1");
+        }
+    });
+
+    Template.login.events({
+        'click button': function () {
+            // increment the counter when button is clicked
+            Session.set("counter1", Session.get("counter1") + 1);
+        }
+    });
+    
+    Template.register.helpers({
+        counter: function () {
+            return Session.get("counter2");
+        }
+    });
+
+    Template.register.events({
+        'click button': function () {
+            // increment the counter when button is clicked
+            Session.set("counter2", Session.get("counter2") + 1);
+        }
+    });
+
+}
+
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+    // code to run on server at startup
+    });
+}

6ca5cf744205444e86f42f1001d080f5ec114cf9
caranapen <carana.pen@gmail.com>
2014-11-27 15:16:53 +0100

    Arreglado el el codigo y lista de usuarios que tenemos en la Meteor.users

--- EnigmaCode/client/client.js
+++ EnigmaCode/client/client.js
@@ -1,26 +1,16 @@
-<<<<<<< HEAD
-=======
-    Meteor.startup(function () {
+
+Meteor.startup(function () {
 			//$('#menu_principal').hide();
 			//$('#container_lateral1').hide();
 			//$('#principal').hide();					
-    });
+});
 
-
-
-   /* Session.setDefault("counter1", 0);
-    Session.setDefault("counter2", 0);
+Template.userlist.users = function(){
+	return Meteor.users.find({},{sort:{username:1}});
+}
+ 
     
-    Template.login.helpers({
-        counter: function () {
-            return Session.get("counter1");
-        }
-    });
->>>>>>> 800064d94053cb725a9391c539c6a7df68193353
-
-
-
 /*  Configuration of signup */
-    Accounts.ui.config({
-  	    passwordSignupFields: "USERNAME_AND_OPTIONAL_EMAIL"
-	});
+Accounts.ui.config({
+	passwordSignupFields: "USERNAME_AND_OPTIONAL_EMAIL"
+});

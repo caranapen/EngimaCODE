6a3d071c5048a3bb1884ddea4dc238abc945b727
Daniel Rodca <dani.kn17.rjch@gmail.com>
2014-11-27 09:20:48 -0400

    Actualizacion html y css

--- EnigmaCode/client/client.js
+++ EnigmaCode/client/client.js
@@ -1,33 +1,7 @@
-   /* Session.setDefault("counter1", 0);
-    Session.setDefault("counter2", 0);
-    
-    Template.login.helpers({
-        counter: function () {
-            return Session.get("counter1");
-        }
-    });
 
-    Template.login.events({
-        'click button': function () {
-            // increment the counter when button is clicked
-            Session.set("counter1", Session.get("counter1") + 1);
-        }
-    });
-    
-    Template.register.helpers({
-        counter: function () {
-            return Session.get("counter2");
-        }
-    });
 
-    Template.register.events({
-        'click button': function () {
-            // increment the counter when button is clicked
-            Session.set("counter2", Session.get("counter2") + 1);
-        }
-    });*/
- 
 
-   Accounts.ui.config({
+/*  Configuration of signup */
+    Accounts.ui.config({
   	    passwordSignupFields: "USERNAME_AND_OPTIONAL_EMAIL"
 	});

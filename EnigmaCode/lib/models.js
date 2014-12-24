d1ed174bbdba1fb2bcec1b51cf6f667f994a991b
Daniel Rodca <dani.kn17.rjch@gmail.com>
2014-12-24 06:48:02 -0400

    Subo mi ultima actualizacion. Estadisticas y mas cosas y cambios gorditos. Espero que el merge no de problemas. Miedo me da

--- EnigmaCode/lib/models.js
+++ EnigmaCode/lib/models.js
@@ -5,4 +5,9 @@ Messages = new Meteor.Collection('messages');
 
 Gameplays = new Meteor.Collection('gameplays');
 
-Stats = new Meteor.Collection('stats');
+Estadisticas = new Meteor.Collection('estadisticas');
+
+//Comentarios para pheras
+//No puedo poner (Meteor.userId()) en el Starup del servidor sabre porque
+//Mi error en la coleccion era que usaba la palabra supongo reservada Stats
+//Reusar funcione declaradas antes dentro de la misma funcion

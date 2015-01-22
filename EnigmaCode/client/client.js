e3aa5474fbc0d036d98a94afea12da7067c2da8c
crismartin <cris.martinz.urjc@hotmail.es>
2015-01-22 12:43:27 +0100

    mejorando bugg exit-creator-partida

--- EnigmaCode/client/client.js
+++ EnigmaCode/client/client.js
@@ -20,13 +20,12 @@ Tracker.autorun(function(){
 	current_game = Session.get('partida_actual');
 	game = Gameplays.findOne({_id: current_game});
 	if (game === undefined){	
-		if (aux_inicio === true){
-			if (aux_creat_id !== Meteor.userId()){
-				alert ("La partida ha sido borrada por marcha del creador");
-				changeView('partidas');
-			}	
+		if ((aux_inicio === true) && (aux_creat_id !== Meteor.userId())){
+			alert ("La partida ha sido borrada por marcha del creador");
+			changeView('partidas');
 		}
 		changeView('partidas');
+
 	}else{
 		aux_creat_id = game.creator_id;
 	}
